dba8acf5b8a5de69cb0857bc83327504964783a2
Tim McKenna <mckenna.tim@gmail.com>
2013-06-03 16:34:01 -0400

    from Sitebuilt5

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
 console.log('This would be the main JS file.');
+$("#hvac").load("http://sitebuilt.net/wiki/Online_heating_system_control_project-desc?useformat=mobile", function (response, status, xhr) {
+if (status == “error”) {
+var msg = “Error: “;
+alert(msg + xhr.status + ” “ + xhr.statusText);
+}
+});
